c8d5ac2ed994c570e0253b8edb0843851be2a5c4
Martin Kunc <5441732+martinkunc@users.noreply.github.com>
2023-04-29 18:59:16 +0200

    Initial commit

---
+++ ConsoleApp1/Calculator.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    internal class Calculator
+    {
+
+
+        public static int Calculate(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ ConsoleApp1/Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    [TestFixture]
+    public class Test
+    {
+        [TestCase(1, 2, 3)]
+        public void TestCalculate(int a, int b, int c ) {
+            Assert.AreEqual(c, Calculator.Calculate(a, b));
+        }
+    }
+}
